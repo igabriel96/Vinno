5a56863d04cd8f7c74a574cbb26b120a7a915099
ioneseigabriel <ioneseigabriel@gmail.com>
2018-01-16 14:16:55 +0200

    adauga doar link-uri de youtube

--- Design/add_annotation.js
+++ Design/add_annotation.js
@@ -1,12 +1,24 @@
 var tab_title = '';
 function display_h1 (results){
-  h1=results;
-  document.querySelector("#id1").innerHTML = "<p>tab title: " + tab_title + "</p><p>dom h1: " + h1 + "</p>";
+  source=document.getElementById('video');
+//console.log(results);
+  var url=results[0];
+ // var url="https://www.youtube.com/embed/MtJ0lrIGSAE"
+  var url=url.replace("watch?v=", "embed/");
+if(url.indexOf('www.youtube') !== -1)
+{
+//daca link-ul este unul de youtube
+  source.setAttribute('src', url);
+}
+else
+{
+	source.setAttribute('src', "https://www.youtube.com/watch?v=MtJ0lrIGSAE");
+}
 }
 chrome.tabs.query({active: true}, function(tabs) {
   var tab = tabs[0];
   tab_title = tab.title;
   chrome.tabs.executeScript(tab.id, {
-    code: 'document.querySelector("h1").textContent'
+    code: 'document.URL'
   }, display_h1);
 });
