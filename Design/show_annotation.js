d6c69b971c7a45d6d70159588dd9bdc1deddb17e
igabriel96 <ioneseigabriel@gmail.com>
2018-01-27 17:39:02 +0200

    Afisare adnotare salvata

---
+++ Design/show_annotation.js
@@ -0,0 +1,29 @@
+var tab_title = '';
+var URL =''
+
+function display_h1 (results) {
+  
+
+  //console.log(results);
+  URL = results[0];
+  URL = URL.replace("watch?v=", "embed/");
+    chrome.storage.sync.get([URL], function(items) {
+      var person = {firstName:"John", lastName:"Doe", age:46};
+      item=items[[URL]];
+      console.log(item['tags']);
+      document.getElementById('annotation_name').innerHTML=item['annotation_name'];
+      document.getElementById('tags').innerHTML=item['tags'];
+      document.getElementById('geo_cord').innerHTML=item['geo_cord'];
+      document.getElementById('link').innerHTML=item['link'];
+      document.getElementById('notes').innerHTML=item['notes'];
+      
+  });
+}
+
+chrome.tabs.query({active: true}, function(tabs) {
+  var tab = tabs[0];
+  tab_title = tab.title;
+  chrome.tabs.executeScript(tab.id, {
+    code: 'document.URL'
+  }, display_h1);
+});
