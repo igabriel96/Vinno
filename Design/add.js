5fa1af80e5b267f3473942ce4c47e0a1281b4477
JBeni <jitca.beniamin@gmail.com>
2018-01-29 10:56:18 +0200

    Add Annot

--- Design/add.js
+++ Design/add.js
@@ -1,26 +1,23 @@
 var tab_title = '';
 var URL = '';
 
-function display_h1 (results) {
+function display_h1(results) {
   source = document.getElementById('video');
 
-  //console.log(results);
   var url = results[0];
-  // var url="https://www.youtube.com/embed/MtJ0lrIGSAE"
   var url = url.replace("watch?v=", "embed/");
   URL = url;
+
   if (url.indexOf('www.youtube') !== -1) {
-    //daca link-ul este unul de youtube
     source.setAttribute('src', url);
   }else if (url.indexOf('www.vimeo') !== -1) {
     source.setAttribute('src', url);
     source.setAttribute('src', "https://vimeo.com/channels/staffpicks/194312144");
   }else {
-     source.setAttribute('src', "https://www.youtube.com/watch?v=MtJ0lrIGSAE");
+     source.setAttribute('src', "https://www.youtube.com/embed/MtJ0lrIGSAE");
   }
 }
 
-//add handler for submit button
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('submit').addEventListener('click', saveAnnotation);
 });
@@ -40,19 +37,9 @@ function saveAnnotation() {
       'geo_cord': document.getElementById('geo_cord').value,
       'link': document.getElementById('link').value,
       'notes': document.getElementById('notes').value,
+      'videoURL': document.URL,
   };
 
-  localStorage.setItem('annotation_name', annotationObject['annotation_name']);
-  localStorage.setItem('tags', annotationObject['tags']);
-  localStorage.setItem('geo_cord', annotationObject['geo_cord']);
-  localStorage.setItem('link', annotationObject['link']);
-  localStorage.setItem('notes', annotationObject['notes']);
-/*
-  var annotationData = JSON.stringify(annotationObject);
-  localStorage.setItem(annotationObject + "-" + index, annotationData);
-  index = index + 1;
-*/
-
+  localStorage.setItem('annotation_' + localStorage.length, JSON.stringify(annotationObject));
   window.location = "popup.html";
-
 }
